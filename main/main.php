e6720621e978f5c2cf208bf271b70fd45e31371c
drkh <abhinandnmurali@gmail.com>
2023-04-12 11:44:53 +0530

    Update main.php

--- main/main.php
+++ main/main.php
@@ -1,26 +1,28 @@
 <?php
-	session_start();
-	if (isset($_SESSION["username"])) {
-		header("Location: profile.php");
-		exit();
-	}
+session_start();
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Cyberpunk</title>
-	<link rel="stylesheet" type="text/css" href="css/cyberpunk.css">
+  <meta charset="UTF-8">
+  <title>Main Page</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<header>
-		<h1>Cyberpunk</h1>
-	</header>
-	<main>
-		<h1>Welcome to Cyberpunk</h1>
-		<p>Please <a href="login.php">login</a> or <a href="signup.php">signup</a> to continue.</p>
-	</main>
+  <div class="header">
+    <?php if(isset($_SESSION['username'])) { ?>
+      <a href="profile.php">Profile</a>
+      <a href="submit_flag.php">Submit Flag</a>
+      <a href="logout.php">Logout</a>
+    <?php } else { ?>
+      <a href="login.php">Login</a>
+      <a href="signup.php">Sign Up</a>
+      <a href="submit_flag.php">Submit Flag</a>
+    <?php } ?>
+  </div>
+  <div class="content">
+    <h1>Welcome to the Main Page</h1>
+    <p>Here, you can access your profile and submit flags for the CTF.</p>
+  </div>
 </body>
 </html>
